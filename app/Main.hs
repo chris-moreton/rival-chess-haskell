d0b6f4299d9580f93c33ef6af283a273f9c7aeb7
Chris Moreton <chris.moreton@hmcts.net>
2021-05-19 10:14:16 +0100

    Adding more UCI structure

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,54 @@
 module Main where
 
 import Data.List.Split ( splitOn )
+import System.Exit
+
+data UCIState = UCIState {
+      position :: String
+    , quit :: Bool
+    , errorMessage :: String
+}
 
 main :: IO ()
 main = do
+  let uciState = UCIState {position = "", quit=False, errorMessage=""}
   putStrLn "Hello"
   command <- getLine
-  let parts = splitOn " " command
-  run parts
+  uciState' <- run uciState (splitOn " " command)
+  let e = errorMessage uciState'
+  putStrLn e
+  if (quit uciState') 
+      then do
+          putStrLn "Bye"
+          exitSuccess
+      else if (e /= "")
+            then putStrLn e
+            else main
+  main
+                
 
-run :: [String] -> IO ()
-run ("hello":xs) = do
-    putStrLn $ head xs
-run (x:xs) = do
+run :: UCIState -> [String] -> IO UCIState
+run uciState ("uci":xs) = do 
+    putStrLn "id name Rival Haskell"
+    putStrLn "id author Chris Moreton"
+    return uciState
+
+run uciState ("position":xs) = runPosition uciState xs
+
+run uciState ("quit":_) = do 
+    return uciState{quit=True}
+
+run uciState (x:xs) = do
     putStrLn x
-    return ()
+    return uciState
+
+runPosition :: UCIState -> [String] -> IO UCIState
+runPosition uciState ("startpos":_) = do
+    return uciState
+
+runPosition uciState ("fen":xs) = do
+    if length xs /= 6
+        then return uciState{errorMessage="Invalid FEN"}
+        else return uciState
+
+
