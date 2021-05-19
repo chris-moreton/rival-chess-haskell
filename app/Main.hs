09ae4a4b5e732365801a845ced28fb5a7e5e1352
Chris Moreton <chris.moreton@hmcts.net>
2021-05-19 01:17:58 +0100

    Start of UCI interface

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,17 @@
 module Main where
 
+import Data.List.Split ( splitOn )
+
 main :: IO ()
 main = do
+  putStrLn "Hello"
   command <- getLine
-  run command
+  let parts = splitOn " " command
+  run parts
 
-run :: String -> IO ()
-run "hello" = do
-    putStrLn "hi"
-run s = do
-    putStrLn s
+run :: [String] -> IO ()
+run ("hello":xs) = do
+    putStrLn $ head xs
+run (x:xs) = do
+    putStrLn x
     return ()
