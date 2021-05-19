687dc1be1efa9b683ea8cf39cfd2f37a45d76f5d
Chris Moreton <chris.moreton@hmcts.net>
2021-05-19 18:03:56 +0100

    Validate fen somewhat

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BlockArguments #-}
+
 module Main where
 
 import Data.List.Split ( splitOn )
@@ -20,14 +22,16 @@ main = do
   command <- getLine
   uciState' <- run uciState (splitOn " " command)
   let e = errorMessage uciState'
-  putStrLn e
   if (quit uciState') 
       then do
           putStrLn "Bye"
           exitSuccess
       else do
-          print ("EnPassant Square is " ++ (show (enPassantSquare (position uciState'))))
-          main     
+          if (e == "") 
+             then print ("EnPassant Square is " ++ (show (enPassantSquare (position uciState'))))
+             else putStrLn e
+          main   
+
 
 run :: UCIState -> [String] -> IO UCIState
 run uciState ("uci":xs) = do 
@@ -44,16 +48,27 @@ run uciState (x:xs) = do
     putStrLn x
     return uciState
 
+verifyFen :: String -> String
+verifyFen s = do
+    let parts = splitOn " " s
+    let len = length parts
+    if (len /= 6)
+        then "Invalid FEN: Expected 6 parts, found " ++ (show len)
+        else ""
+
 runPosition :: UCIState -> [String] -> IO UCIState
 runPosition uciState ("startpos":xs) = runPosition uciState (["fen",startPosition] ++ xs)
 
 runPosition uciState ("fen":xs) = do
     let parts = splitOn " moves " $ stringArrayToWords xs
     let fen = head parts
-    print $ show fen
-    let moves = splitOn " " (head (tail parts))
-    print $ show moves
-    return uciState{position=makeMoves (getPosition fen) moves}
+    let error = verifyFen fen
+    if error == ""
+        then do
+            let moves = splitOn " " (head (tail parts))
+            return uciState{position=makeMoves (getPosition fen) moves, errorMessage=""}
+        else do
+            return uciState{errorMessage=error}
 
 makeMoves :: Position -> [String] -> Position
 makeMoves position [] = position
