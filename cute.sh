87894ea95cda861d9abbbe58ec957e82939d9aca
Chris Moreton <chris@netsensia.com>
2021-05-24 00:39:31 +0100

    Adding alpha/beta to searchZero

--- cute.sh
+++ cute.sh
@@ -1,2 +1,2 @@
-cutechess-cli -engine cmd=exe/rival-$1.exe nodes=1000 -engine cmd=exe/rival-$2.exe -each st=0.01 proto=uci timemargin=15000 book=exe/ProDeo.bin -rounds $3 -pgnout "out.pgn" -debug
+cutechess-cli -engine cmd=exe/rival-$1.exe nodes=1000 -engine cmd=exe/rival-$2.exe -each st=$4 proto=uci timemargin=15000 book=exe/ProDeo.bin -rounds $3 -pgnout "out.pgn" -debug
 
