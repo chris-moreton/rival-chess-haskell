7f4b7304a3bfc068e445ddecd27ee1fca03f0f48
Chris Moreton <chris@netsensia.com>
2021-07-07 11:45:45 +0100

    Stamping as build 600

--- app/Main.hs
+++ app/Main.hs
@@ -37,7 +37,7 @@ main = do
 
 showId :: IO ()
 showId = do
-    putStrLn "id name Rival Haskell Build 201"
+    putStrLn "id name Rival Haskell Build 600"
     putStrLn "id author Chris Moreton"
     putStrLn "uciok"
 
