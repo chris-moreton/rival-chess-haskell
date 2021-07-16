e7ac028f247c2922a7c27f405b87eac6fe00ba9f
Chris Moreton <chris@netsensia.com>
2021-07-16 16:24:15 +0100

    Stamping as build 804

--- app/Main.hs
+++ app/Main.hs
@@ -38,7 +38,7 @@ main = do
 
 showId :: IO ()
 showId = do
-    putStrLn "id name Rival Haskell Build 800"
+    putStrLn "id name Rival Haskell Build 804"
     putStrLn "id author Chris Moreton"
     putStrLn "uciok"
 
